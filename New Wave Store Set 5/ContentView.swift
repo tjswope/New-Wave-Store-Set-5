afed44e808a054c2b3e6253b0c9c3a5e376919c2
Tom Swope <tjswope@gmail.com>
2022-11-18 12:56:19 -0500

    Initial Commit

---
+++ New Wave Store Set 5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  New Wave Store Set 5
+//
+//  Created by Swope, Thomas on 11/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ New Wave Store Set 5/New_Wave_Store_Set_5App.swift
@@ -0,0 +1,17 @@
+//
+//  New_Wave_Store_Set_5App.swift
+//  New Wave Store Set 5
+//
+//  Created by Swope, Thomas on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct New_Wave_Store_Set_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
