0ec5cbacb4f4d091c1e40045ccb3507527d6cfb8
Tom Swope <tjswope@gmail.com>
2022-12-01 12:08:44 -0500

    added cart class.

---
+++ New Wave Store Set 5/Model/Cart.swift
@@ -0,0 +1,18 @@
+//
+//  Cart.swift
+//  New Wave Store Set 5
+//
+//  Created by Swope, Thomas on 12/1/22.
+//
+
+import Foundation
+
+class Cart{
+    var products: ProductList = ProductList(products: [])
+    
+    var total: Double {
+        // iterate through the products in productlist and sum each price * quantity
+        
+        return 0.0
+    }
+}
--- New Wave Store Set 5/Model/ProductList.swift
+++ New Wave Store Set 5/Model/ProductList.swift
@@ -10,7 +10,9 @@ import Foundation
 class ProductList: ObservableObject{
     @Published var products: [Product]
     
-    init(){
-        products = [Product(), Product(name: "nwPhone", description: "", picture: "wPhone", price: 1999.50, quantity: 3000), Product(name: "wPen", description: "", picture: "wPen", price: 4999.99, quantity: 10000), Product(name: "Software", description: "", picture: "software", price: 450.50, quantity: 250)]
+    init(products : [Product] = [Product(), Product(name: "nwPhone", description: "", picture: "wPhone", price: 1999.50, quantity: 3000), Product(name: "wPen", description: "", picture: "wPen", price: 4999.99, quantity: 10000), Product(name: "Software", description: "", picture: "software", price: 450.50, quantity: 250)]){
+        
+        self.products = products
+        
     }
 }
